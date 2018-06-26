d17221ce6d90f88e5ea598f999a0b4f2efe39376
jesberman <jesberman@gmail.com>
2018-06-25 21:15:24 -0400

    added_landing_page

---
+++ Assets/javascript/logic.js
@@ -0,0 +1,30 @@
+$('.slide-wrapper').carousel({
+    pause: "false"
+});
+
+
+
+
+
+$('#al').click(function(){
+    window.location.href = "auto_location.html";
+    return false; });
+
+
+
+$('#uil').click(function(){
+    window.location.href = "user_input_location.html";
+    return false; });
+ 
+
+
+
+
+
+
+
+
+
+
+
+
